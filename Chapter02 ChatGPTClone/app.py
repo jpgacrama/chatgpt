91e1120d03467a8c208575f7d74edafab0bf1faf
Jonas Gacrama <jonasgacrama@gmail.com>
2023-11-14 17:53:05 +0800

    Chapter02: Removed hard-coded copy of the API key from Version Control

--- Chapter02 ChatGPTClone/app.py
+++ Chapter02 ChatGPTClone/app.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from openai import OpenAI
+import config
+import os
+
+def clear_screen():
+    # Clear the screen based on the operating system
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear_screen()
+
+client = OpenAI(
+    api_key = config.API_KEY,
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello, World!"
-
 if __name__ == "__main__":
     app.run()
