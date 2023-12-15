a8da9de5c2f24d6160f0abcbc27aae9ba97f6cef
Jonas Gacrama <jonasgacrama@gmail.com>
2023-12-15 18:49:42 +0800

    Chapter05: Finished creating the Quiz Generator app

--- Chapter05 QuizApp/quiz_project/quiz_project/urls.py
+++ Chapter05 QuizApp/quiz_project/quiz_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz_app.urls')),
+    path('history/', include('quiz_app.urls')),
 ]
